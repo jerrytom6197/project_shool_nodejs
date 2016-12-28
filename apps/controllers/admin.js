b45e8e2609e2d6a164cb73ff6f57e13bb4f94947
lttai <jerrytom@gamil.com>
2016-12-29 06:01:59 +0700

    up

--- apps/controllers/admin.js
+++ apps/controllers/admin.js
@@ -2,19 +2,28 @@ var express = require("express");
 var router = express.Router();
 var config = require('config');
 
-var data = {
-    title: 'Login | '+config.get('domain.nameurl'),
-    url: config.get('domain.url')
-}
+
 
 router.get("/", function (req, res) {
-  res.render("admin/dashboard", {title: "Dashboard"});
+    var data = {
+        title: 'Dashboard | '+config.get('domain.nameurl'),
+        url: config.get('domain.url')
+    }
+  res.render("admin/dashboard", data);
 });
 
+
+
 router.get('/login', function(req, res){
-    title
+    var data = {
+        title: 'Login | '+config.get('domain.nameurl'),
+        url: config.get('domain.url')
+    }
   res.render("admin/login", data);
 });
 
+function get (a) {
+
+}
 
 module.exports = router;
